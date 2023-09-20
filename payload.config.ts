a6ce74d066ff7c13f06f332f1b48514a07b4877e
EthanNC <ethan.n.cumming@gmail.com>
2023-09-20 18:41:39 -0400

    add deployment url to config deployment

--- payload.config.ts
+++ payload.config.ts
@@ -3,12 +3,18 @@ import path from "path";
 import Users from "./cms/collections/Users";
 
 export default buildConfig({
-  serverURL: "http://localhost:3000",
+  cors: process.env.RAILWAY_PUBLIC_DOMAIN
+    ? [process.env.RAILWAY_PUBLIC_DOMAIN]
+    : [],
+  csrf: process.env.RAILWAY_PUBLIC_DOMAIN
+    ? [process.env.RAILWAY_PUBLIC_DOMAIN]
+    : [],
+  serverURL: process.env.RAILWAY_PUBLIC_DOMAIN ?? "http://localhost:3000",
   admin: {
     user: Users.slug,
   },
   collections: [Users],
   typescript: {
     outputFile: path.resolve(__dirname, "cms/payload-types.ts"),
-  }
+  },
 });
